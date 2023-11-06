a2ff39bab77e3a04adf406dae1bb9726f8f93732
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-11-06 11:43:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
-from tkinter import Tk, Button, Entry
+from tkinter import Tk, Button, Entry, END
 
 # Configuración ventana principal
 root = Tk()
 root.title("Calculadora POO")
 root.resizable(0,0)
-root.geometry("400x300")
+root.geometry("510x260")
 
 # Configuración pantalla de salida 
 pantalla = Entry(root, width=40, bg="black", fg="white", borderwidth=0, font=("arial", 18, "bold"))
-pantalla.grid(row=0, column=0, columnspan=***, padx=1, pady=***)
+pantalla.grid(row=0, column=0, columnspan=4, padx=0, pady=1)
+
+x = 0
+
+def dig(num):
+    global x
+    pantalla.insert(x, num)
+    x+=1
+
+def resul():
+    op= pantalla.get()
+    resul = eval(op) 
+    pantalla.delete(0, END)
+    pantalla.insert(0, resul)
+    x=0
+
 
 # Configuración botones
-boton_1 = Button(root, text="1", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=1, column=***, padx=1, pady=***)
-boton_2 = Button(root, text="2", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=***, pady=1)
-boton_3 = Button(root, text="3", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=1, column=***, padx=***, pady=1)
-boton_4 = Button(root, text="4", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=***)
-boton_5 = Button(root, text="5", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=1, padx=***, pady=***)
-boton_6 = Button(root, text="6", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=2, column=***, padx=***, pady=1)
-boton_7 = Button(root, text="7", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=***)
-boton_8 = Button(root, text="8", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=1)
-boton_9 = Button(root, text="9", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=3, column=2, padx=***, pady=***)
-boton_igual = Button(root, text="=", width=20, height=3, bg="red", fg="white", borderwidth=0, cursor=" hand2").grid(row=4, column=***, columnspan=***, padx=1, pady=1)
-boton_punto = Button(root, text=".", width=9, height=3, bg="spring green", fg="black", cursor="hand2", borderwidth=0).grid(row=***, column=***, padx=1, pady=1)
-boton_mas = Button(root, text="+", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=1, column=***, padx=***, pady=1)
-boton_menos = Button(root, text="-", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=***, column=3, padx=1, pady=1)
-boton_multiplicacion = Button(root, text="*",  width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=1)
-boton_division = Button(root, text="/", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=***)
+boton_1 = Button(root, text="1", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command= lambda:dig(1)).grid(row=1, column=0, padx=1, pady=1, sticky="nsew")
+boton_2 = Button(root, text="2", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command= lambda:dig(2)).grid(row=1, column=1, padx=1, pady=1, sticky="nsew", )
+boton_3 = Button(root, text="3", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command= lambda:dig(3)).grid(row=1, column=2, padx=1, pady=1, sticky="nsew")
+boton_4 = Button(root, text="4", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command= lambda:dig(4)).grid(row=2, column=0, padx=1, pady=1, sticky="nsew")
+boton_5 = Button(root, text="5", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command= lambda:dig(5)).grid(row=2, column=1, padx=1, pady=1, sticky="nsew")
+boton_6 = Button(root, text="6", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command= lambda:dig(6)).grid(row=2, column=2, padx=1, pady=1, sticky="nsew")
+boton_7 = Button(root, text="7", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command= lambda:dig(7)).grid(row=3, column=0, padx=1, pady=1, sticky="nsew")
+boton_8 = Button(root, text="8", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command= lambda:dig(8)).grid(row=3, column=1, padx=1, pady=1, sticky="nsew")
+boton_9 = Button(root, text="9", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command= lambda:dig(9)).grid(row=3, column=2, padx=1, pady=1, sticky="nsew")
+boton_igual = Button(root, text="=", width=20, height=3, bg="red", fg="white", borderwidth=0, cursor=" hand2", command=lambda:resul()).grid(row=4, column=0, columnspan=2, padx=1, pady=1, sticky="nsew")
+boton_punto = Button(root, text=".", width=9, height=3, bg="spring green", fg="black", cursor="hand2", borderwidth=0, command= lambda:dig(".")).grid(row=4, column=2, padx=1, pady=1, sticky="nsew")
+boton_mas = Button(root, text="+", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command= lambda:dig("+")).grid(row=1, column=3, padx=1, pady=1, sticky="nsew")
+boton_menos = Button(root, text="-", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command= lambda:dig("-")).grid(row=2, column=3, padx=1, pady=1, sticky="nsew")
+boton_multiplicacion = Button(root, text="*",  width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command= lambda:dig("*")).grid(row=3, column=3, padx=1, pady=1, sticky="nsew")
+boton_division = Button(root, text="/", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command= lambda:dig("/")).grid(row=4, column=3, padx=1, pady=1, sticky="nsew")
+
 
 root.mainloop()
